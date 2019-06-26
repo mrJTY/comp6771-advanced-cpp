a9db102027d55e55fc79e0efcad26486a444fb3a
jty <justin.ty@rokt.com>
2019-06-26 15:36:28 +1000

    Compound assignment

--- lectures/week4/compass.cpp
+++ lectures/week4/compass.cpp
@@ -1,11 +1,18 @@
+#include <ostream>
+#include <iostream>
+
 // Point.h:
 class Point {
   public:
+    Point(int x, int y) : x_{x}, y_{y} {};
+
     Point& operator+=(const Point& p);
     Point& operator-=(const Point& p);
     Point& operator*=(const Point& p);
     Point& operator/=(const Point& p);
     Point& operator*=(const int& i);
+    const int& x() const { return this->x_; };
+    const int& y() const { return this->y_; };
 
   private:
     int x_;
@@ -16,9 +23,24 @@ class Point {
 Point& Point::operator+=(const Point& p) {
   this->x_ += p.x_;
   this->y_ += p.y_;
+  return *this; // this is a pointer. Return the actual object with *this
+}
+
+Point& Point::operator-=(const Point& p) {
+  this->x_ -= p.x_;
+  this->y_ -= p.y_;
   return *this;
 }
-Point& Point::operator-=(const Point& p) { /* Should we do this one? */ }
-Point& Point::operator*=(const Point& p) { /* Should we do this one? */ }
-Point& Point::operator/=(const Point& p) { /* Should we do this one? */ }
-Point& Point::operator*=(const int& p) { /* Should we do this one? */ }
+//Point& Point::operator-=(const Point& p) { /* Should we do this one? */ }
+//Point& Point::operator*=(const Point& p) { /* Should we do this one? */ }
+//Point& Point::operator/=(const Point& p) { /* Should we do this one? */ }
+//Point& Point::operator*=(const int& p) { /* Should we do this one? */ }
+
+int main(){
+  Point p1 {1,2};
+  Point p2 {3,4};
+
+  p1 += p2;
+  std::cout << p1.x() << " " << p1.y() << std::endl;
+
+}
--- lectures/week4/point2.cpp
+++ lectures/week4/point2.cpp
@@ -4,6 +4,12 @@
 class Point {
   public:
     Point(int x, int y) : x_{x}, y_{y} {};
+
+    /**
+    * Friend: not associated with the class but has private access
+    * to member fields
+    *
+    **/
     friend Point operator+(const Point& lhs, const Point& rhs);
     friend std::ostream& operator<<(std::ostream& os,
                                     const Point& p);
@@ -12,17 +18,27 @@ class Point {
     int y_;
 };
 
+/**
+ * This is copied to a new point object
+ **/
 Point operator+(const Point& lhs, const Point& rhs) {
   return Point{lhs.x_ + rhs.x_, lhs.y_ + rhs.y_};
 }
 
+/**
+ * So that we can use the << operator for any output stream
+ **/
 std::ostream& operator<<(std::ostream& os, const Point& p) {
-  os << "(" << p.x_ << "," << p.y_ << ")";
+  os << "[" << p.x_ << " , " << p.y_ << "]";
   return os;
 }
 
 int main() {
   Point p1{1, 2};
   Point p2{2, 3};
+
+  /**
+   * Cleaner operator
+   **/
   std::cout << p1 + p2 << "\n";	
 }
