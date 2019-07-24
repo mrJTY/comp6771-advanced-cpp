273c94bdf1fa5cfdc4c5f34c4a4fe4e594171768
jtdv01 <jtdv01@gmail.com>
2019-07-24 16:26:13 +1000

    Stack of types

--- tutorials/week8/node.h
+++ tutorials/week8/node.h
@@ -11,7 +11,8 @@
 template<typename T>
 struct Node{
 public:
-    Node(T value, std::unique_ptr<Node>&& next) : value_(value), next_(std::move(next)){}
+    //Node(T value) : value_(value) {}
+    Node(T value, std::unique_ptr<Node<T>>&& next) : value_(value), next_(std::move(next)){}
 
     T value_;
     std::unique_ptr<Node> next_;
@@ -31,7 +32,7 @@ public:
         return node_->value_;
     };
     MyIter& operator++(){
-        // TODO: node_ = node_->next.get();
+        node_ = node_->next;
     };
     MyIter operator++(int){
         auto copy{*this};
@@ -47,6 +48,32 @@ public:
 
     private:
         Node<T>* node_;
+};
+
+template<typename T>
+class Stack{
+public:
+    // TODO(lecture): show how make const and non-const iterators during lecture.
+    using iterator = MyIter<T>;
+    //using const_iterator = Iterator;
+
+    iterator begin() { return iterator{head_.get()}; }
+//    const_iterator begin() const { return cbegin(); }
+    //const_iterator cbegin() const { return const_iterator{head_.get()}; }
+    iterator end() { return iterator{nullptr}; }
+    //const_iterator end() const { return cend(); }
+    //const_iterator cend() const { return const_iterator{nullptr}; }
+
+    void push(T value) { head_ = std::make_unique<Node<T>>(value, std::move(head_)); }
+
+    // TODO(students): Why doesn't std::stack::pop return the value you pop?
+    void pop() { head_ = std::move(head_->next); }
+
+    //const int& top() const { return *cbegin(); }
+    T& top() { return *begin(); }
+
+private:
+    std::unique_ptr<Node<T>> head_;
 };
 
 #endif //COMP6771_ADVANCED_CPP_NODE_H
--- tutorials/week8/node_user.cpp
+++ tutorials/week8/node_user.cpp
@@ -4,11 +4,27 @@
 
 #include "string"
 #include "iostream"
+#include <memory>
 #include "tutorials/week8/node.h"
 
 int main(){
-    std::string hellostr = "hello";
-    Node<std::string> node{hellostr, nullptr };
-    std::cout << node.value_;
+//    std::string hellostr = "hello";
+//    std::string worldstr = "world";
+//
+//    std::unique_ptr<Node<std::string>> ptr1 = std::make_unique<Node<std::string>>(hellostr);
+//    std::unique_ptr<Node<std::string>> ptr2 = std::make_unique<Node<std::string>>(worldstr);
+//
+//    Node<std::string> node1{hellostr, ptr2};
+//    Node<std::string> node2{worldstr, ptr1};
+
+
+    Stack<std::string> s;
+    s.push("asdf");
+    s.push("foo");
+    std::cout << s.top();
+
+
+
+
     return 0;
 }
